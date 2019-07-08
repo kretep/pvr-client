7fca60168f781fe85e756bfbeb483767aa5fe762
Peter Kok <kretep@users.noreply.github.com>
2019-07-08 13:41:25 +0200

    Only show copy button for new persons

--- src/VisitDetails.js
+++ src/VisitDetails.js
@@ -212,7 +212,7 @@ class VisitDetails extends React.Component {
   }
 
   render () {
-    const { loading, error, message, visit, person, isAdmin } = this.state;
+    const { loading, error, message, visit, person, isAdmin, isNew } = this.state;
     const phonePattern = /(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)/.source;
     const postcodePattern = /^[1-9]\d{3}\s?[a-zA-Z]{2}(\s?\d+(\S+)?)?$/.source;
     //const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.source;
@@ -233,9 +233,9 @@ class VisitDetails extends React.Component {
             autoComplete="off" noValidate
             onSubmit={this.onFormSubmit.bind(this)}>
 
-            <div className="offset-sm-3 col-sm-9 form-group">
+            { isNew && <div className="offset-sm-3 col-sm-9 form-group">
               <button type="button" className="btn btn-secondary btn-lg" disabled={!this.global.previousPerson} onClick={this.copyFromPreviousPerson.bind(this)}>Kopieer van vorig kind</button>
-            </div>
+            </div> }
 
             <div className="col-sm-12">
               { isAdmin && <FormInput name="date" label="Datum:" value={ visit.date } /> }
