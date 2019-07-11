290cfa6ee7b9e8b1720db50375d5971753f63a4a
Peter Kok <kretep@users.noreply.github.com>
2019-07-11 12:32:54 +0200

    Add client and server version to info page

--- src/Info.js
+++ src/Info.js
@@ -3,16 +3,31 @@ import { showLocalIP } from './network';
 
 export default class Info extends React.Component {
   state = {
+    serverVersion: {}
   }
   
   componentDidMount() {
     showLocalIP();
+    fetch('/version')
+      .then(response => response.json())
+      .then(response => { 
+        this.setState({ serverVersion: response })
+      });
   }
 
   render () {
+    const { serverVersion } = this.state;
     return (
     <div>
-      This client's local address: <span id="localIP">unknown</span>
+      This client's local address: <span id="localIP">unknown</span><br />
+      <br />
+      <strong>{process.env.REACT_APP_NAME}</strong><br />
+      Version: {process.env.REACT_APP_VERSION}<br />
+      Git head: {process.env.REACT_APP_GITHEAD}<br />
+      <br />
+      <strong>{serverVersion.name}</strong><br />
+      Version: {serverVersion.version}<br />
+      Git head: {serverVersion.githead}<br />
     </div>);
   }
 }
